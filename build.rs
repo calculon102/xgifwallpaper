0266043cc9a4dafe12adbf947ad60cbf3d035672
Frank Großgasteiger <frank@pixelgerecht.de>
2020-08-16 14:18:09 +0200

    First POC of shm-usage

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("cargo:rustc-link-lib=X11");
     println!("cargo:rustc-link-lib=Xinerama");
+
+    // For XShm
+    println!("cargo:rustc-link-lib=Xext");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 mod placement;
 mod screen_info;
 
-use clap::{Arg, ArgMatches, App};
+use clap::{App, Arg, ArgMatches};
 
-use gift::Decoder;
 use gift::decode::Steps;
+use gift::Decoder;
 
 use pix::rgb::Rgba8;
 
-use std::ptr;
-use std::rc::Rc;
-use std::ffi:: { CString, c_void };
+use std::ffi::{c_void, CString};
 use std::fs::File;
 use std::io::BufReader;
-use std::os::raw:: { c_char, c_uint };
+use std::mem;
+use std::os::raw::{c_char, c_int, c_uint};
+use std::ptr::{null, null_mut};
+use std::rc::Rc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::{ thread, time };
+use std::{thread, time};
 
 use x11::xlib::*;
+use x11::xshm;
 
-use screen_info::*;
 use placement::*;
+use screen_info::*;
 
 // TODO Find lib to Fill and Scale-Feature images
 // TODO Introduce libxshm for better performance, see
@@ -35,35 +37,33 @@ use placement::*;
 // TODO Bug: Black glitches with some GIFs
 
 struct Frame {
-    image: XImage,
-    raster: Rc<Vec<c_char>>,
     delay: time::Duration,
-    placements: Vec<ImagePlacement>
+    ximage: XImage,
+    placements: Vec<ImagePlacement>,
+    raster: Rc<Vec<c_char>>,
+    xshminfo: Box<xshm::XShmSegmentInfo>,
 }
 
 struct Options<'a> {
     path_to_gif: &'a str,
-    verbose: bool
+    verbose: bool,
 }
 
-fn load_gif(filename: &str) -> Steps<BufReader<File>>
-{
-    let file_in = File::open(filename)
-        .expect("Could not load gif");
+fn load_gif(filename: &str) -> Steps<BufReader<File>> {
+    let file_in = File::open(filename).expect("Could not load gif");
 
     let decoder = Decoder::new(file_in);
 
     return decoder.into_steps();
 }
 
-fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<BufReader<File>>)
-{
+fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<BufReader<File>>) {
     unsafe {
-        let display = XOpenDisplay(ptr::null());
-       
+        let display = XOpenDisplay(null());
+
         let r = running.clone();
         let mut frames = prepare_frames(options.clone(), r, display, steps);
-        
+
         // Single root-loop
         // TODO multi-root implementation
         let screen = XDefaultScreen(display);
@@ -73,25 +73,29 @@ fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<
         let root = XRootWindow(display, screen);
         let depth = XDefaultDepth(display, screen) as u32;
 
-        let pixmap = XCreatePixmap(display, root, display_width as u32, display_height as u32, depth);
+        let pixmap = XCreatePixmap(
+            display,
+            root,
+            display_width as u32,
+            display_height as u32,
+            depth,
+        );
 
         XClearWindow(display, root);
         XSync(display, False);
-               
+
         let screen_info = get_screen_info();
         for i in 0..(frames.len()) {
-            let image_width = frames[i].image.width;
-            let image_height = frames[i].image.height;
+            let image_width = frames[i].ximage.width;
+            let image_height = frames[i].ximage.height;
 
             for screen in &screen_info.screens {
-                frames[i].placements.push(
-                    get_image_placement(
-                        image_width,
-                        image_height,
-                        screen.clone(),
-                        ImagePlacementStrategy::CENTER,
-                    )
-                );
+                frames[i].placements.push(get_image_placement(
+                    image_width,
+                    image_height,
+                    screen.clone(),
+                    ImagePlacementStrategy::CENTER,
+                ));
             }
         }
 
@@ -101,10 +105,10 @@ fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<
                     break;
                 }
 
-                let mut image = frame.image;
+                let mut image = frame.ximage;
 
                 for placement in &frame.placements {
-                    XPutImage(
+                    xshm::XShmPutImage(
                         display,
                         pixmap,
                         gc,
@@ -113,8 +117,9 @@ fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<
                         placement.src_y,
                         placement.dest_x,
                         placement.dest_y,
-                        placement.width as c_uint, 
-                        placement.height as c_uint
+                        placement.width as c_uint,
+                        placement.height as c_uint,
+                        False,
                     );
                 }
 
@@ -123,7 +128,7 @@ fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<
                 }
 
                 XSetWindowBackgroundPixmap(display, root, pixmap);
-      
+
                 XSync(display, False);
 
                 thread::sleep(frame.delay);
@@ -138,15 +143,13 @@ fn loop_animation(options: Arc<Options>, running: Arc<AtomicBool>, steps: Steps<
 
 fn prepare_frames(
     options: Arc<Options>,
-    running: Arc<AtomicBool>, 
-    xdisplay: *mut Display, 
-    frames: Steps<BufReader<File>>
-    ) -> Vec<Frame>
-{
+    running: Arc<AtomicBool>,
+    xdisplay: *mut Display,
+    frames: Steps<BufReader<File>>,
+) -> Vec<Frame> {
     let mut out: Vec<Frame> = Vec::new();
-
     let mut frame_count = 0;
-    
+
     for step_option in frames {
         if !running.load(Ordering::SeqCst) {
             break;
@@ -155,94 +158,160 @@ fn prepare_frames(
         let step = step_option.expect("Empty step in animation");
         let raster = step.raster();
 
+        let width = raster.width();
+        let height = raster.height();
+
         if options.verbose {
             frame_count = frame_count + 1;
-            println!("Convert step {} to XImage, delay: {:?}, width: {}, height: {}", frame_count, step.delay_time_cs(), raster.width(), raster.height());
+            println!(
+                "Convert step {} to XImage, delay: {:?}, width: {}, height: {}",
+                frame_count,
+                step.delay_time_cs(),
+                width,
+                height
+            );
         }
+
+        // Write frame image into owned byte-vector
+        let i8_slice = unsafe { &*(raster.as_u8_slice() as *const [u8] as *const [i8]) };
+        let mut data: Vec<i8> = Vec::with_capacity((width * height * 4) as usize);
+
+        let mut i = 0;
+        let s = 4;
+
+        let prev_raster: Rc<Vec<i8>> = {
+            if out.len() > 0 {
+                out.last().unwrap().raster.clone()
+            } else {
+                // TODO User-defined background-color
+                Rc::new(vec![
+                    0;
+                    raster.width() as usize
+                        * raster.height() as usize
+                        * std::mem::size_of::<Rgba8>()
+                ])
+            }
+        };
+
+        while i < i8_slice.len() {
+            let alpha = i8_slice[i + 3] as u8;
+
+            if alpha == 255 {
+                data.push(i8_slice[i]);
+                data.push(i8_slice[i + 1]);
+                data.push(i8_slice[i + 2]);
+                data.push(i8_slice[i + 3]);
+            } else {
+                data.push(prev_raster[i]);
+                data.push(prev_raster[i + 1]);
+                data.push(prev_raster[i + 2]);
+                data.push(prev_raster[i + 3]);
+            }
+            i += s;
+        }
+
+        let data_ptr: Rc<Vec<i8>> = Rc::new(data);
+
+        let xscreen = unsafe { XDefaultScreenOfDisplay(xdisplay) };
+        let xvisual = unsafe { XDefaultVisualOfScreen(xscreen) };
+
+        let mut xshminfo =
+            create_xshm_sgmnt_inf(data_ptr.clone(), (width * height * 4) as usize).unwrap();
+        let ximage =
+            create_xshm_image(xdisplay, xvisual, &mut xshminfo, width, height, 24).unwrap();
+
+        let data_size = unsafe { ((*ximage).bytes_per_line * (*ximage).height) as usize };
+
+        assert_eq!(
+            data_ptr.len(),
+            data_size,
+            "data-vector must be same length (is {}) as its anticipated capacity and size (is {})",
+            data_ptr.len(),
+            data_size
+        );
 
         unsafe {
-            let xscreen = XDefaultScreenOfDisplay(xdisplay);
-            let xvisual = XDefaultVisualOfScreen(xscreen);
+            // xshminfo.shmaddr = data_ptr.as_ptr() as *mut _;
+            // (*ximage).data = data_ptr.as_ptr() as *mut _;
+            xshm::XShmAttach(xdisplay, xshminfo.as_mut() as *mut _);
+        };
 
-            let ximage = XCreateImage(
-                xdisplay,
-                xvisual,
-                24,
-                ZPixmap,
-                0,
-                ptr::null_mut(), 
-                raster.width(),
-                raster.height(),
-                32,
-                0 
-            );
-
-            let data_size = ((*ximage).bytes_per_line * (*ximage).height) as usize;
-
-            // Have to copy slice to make available to xlib-struct.
-            // Would be better of, making a pointer to slice-data, still.
-            let i8_slice = &*(raster.as_u8_slice() as *const [u8] as *const [i8]);
-            // let mut data = i8_slice.to_vec();
-            let mut data: Vec<i8> = Vec::with_capacity(data_size);
-
-            let mut i = 0;
-            let s = 4;
-
-            // DEBUG
-            let prev_raster: Rc<Vec<i8>> = {
-                if out.len() > 0 {
-                    out.last().unwrap().raster.clone()
-                } else {
-                    // TODO User-defined background-color
-                    Rc::new(vec![0; raster.width() as usize * raster.height() as usize * std::mem::size_of::<Rgba8>()])
-                }
-            };
-
-            while i < i8_slice.len() {
-                let alpha = i8_slice[i+3] as u8;
-
-                if alpha == 255 {
-                    data.push(i8_slice[i]);
-                    data.push(i8_slice[i+1]);
-                    data.push(i8_slice[i+2]); 
-                    data.push(i8_slice[i+3]);
-                } else {
-                    data.push(prev_raster[i]);   
-                    data.push(prev_raster[i+1]);   
-                    data.push(prev_raster[i+2]);   
-                    data.push(prev_raster[i+3]);   
-                }
-                i += s;
-            }
-
-            assert_eq!(data.len(), data_size, 
-                "data-vector must be same length (is {}) as its anticipated capacity and size (is {})", 
-                data.len(), data_size);
-
-            let data_ptr = data.as_mut_ptr();
-            (*ximage).data = data_ptr;       
-   
-            let mut delay = step.delay_time_cs().unwrap_or(10);
-            if delay <= 0  {
-                delay = 10;
-            }
-
-            out.push(Frame {
-                image: *ximage,
-                raster: Rc::new(data),
-                delay: time::Duration::from_millis((delay * 10) as u64), 
-                placements: Vec::new(),
-            });
+        let mut delay = step.delay_time_cs().unwrap_or(10);
+        if delay <= 0 {
+            delay = 10;
         }
+
+        out.push(Frame {
+            delay: time::Duration::from_millis((delay * 10) as u64),
+            placements: Vec::new(),
+            raster: data_ptr,
+            ximage: unsafe { *ximage },
+            xshminfo: xshminfo,
+        });
     }
 
     return out;
 }
 
+fn create_xshm_sgmnt_inf(data: Rc<Vec<i8>>, size: usize) -> Result<Box<xshm::XShmSegmentInfo>, u8> {
+    use libc::size_t;
+    let shmid: c_int =
+        unsafe { libc::shmget(libc::IPC_PRIVATE, size as size_t, libc::IPC_CREAT | 0o777) };
+    if shmid < 0 {
+        return Err(1);
+    }
+    let shmaddr: *mut libc::c_void = unsafe { libc::shmat(shmid, null(), 0) };
+    if shmaddr == ((usize::max_value()) as *mut libc::c_void) {
+        return Err(2);
+    }
+    let mut shmidds: libc::shmid_ds = unsafe { mem::zeroed() };
+    unsafe { libc::shmctl(shmid, libc::IPC_RMID, &mut shmidds) };
+
+    unsafe { libc::memcpy(shmaddr as *mut c_void, data.as_ptr() as *mut _, size) };
+
+    Ok(Box::new(xshm::XShmSegmentInfo {
+        shmseg: 0,
+        shmid,
+        shmaddr: (shmaddr as *mut c_char),
+        readOnly: 0,
+    }))
+}
+
+fn destroy_xshm_sgmnt_inf(seginf: &mut Box<xshm::XShmSegmentInfo>) {
+    unsafe { libc::shmdt(seginf.shmaddr as *mut libc::c_void) };
+}
+
+fn create_xshm_image(
+    dspl: *mut Display,
+    vsl: *mut Visual,
+    xshminfo: &mut Box<xshm::XShmSegmentInfo>,
+    width: u32,
+    height: u32,
+    depth: u32,
+) -> Result<*mut XImage, u8> {
+    unsafe {
+        let ximg = xshm::XShmCreateImage(
+            dspl,
+            vsl,
+            depth,
+            ZPixmap,
+            null_mut(),
+            xshminfo.as_mut() as *mut _,
+            width,
+            height,
+        );
+        if ximg == null_mut() {
+            return Err(1);
+        }
+        (*ximg).data = xshminfo.shmaddr;
+        Ok(ximg)
+    }
+}
+
 // TODO Split into update atoms and remove old atoms
 unsafe fn set_root_atoms(display: *mut Display, root: u64, pixmap: Pixmap) -> bool {
-    let xrootmap_id = CString::new("_XROOTPMAP_ID").expect("Failed!"); 
-    let esetroot_pmap_id = CString::new("ESETROOT_PMAP_ID").expect("Failed!"); 
+    let xrootmap_id = CString::new("_XROOTPMAP_ID").expect("Failed!");
+    let esetroot_pmap_id = CString::new("ESETROOT_PMAP_ID").expect("Failed!");
 
     let mut atom_root = XInternAtom(display, xrootmap_id.as_ptr(), True);
     let mut atom_eroot = XInternAtom(display, esetroot_pmap_id.as_ptr(), True);
@@ -253,30 +322,58 @@ unsafe fn set_root_atoms(display: *mut Display, root: u64, pixmap: Pixmap) -> bo
     // use 0 as direct, known value of None. See X.h.
     if atom_root != 0 && atom_eroot != 0 {
         // TODO Better way to have an initialized, non-null pointer?
-        let data_root = CString::new("").expect("Failed!"); 
-        let mut data_root_ptr : *mut u8 = data_root.as_ptr() as *mut u8;
+        let data_root = CString::new("").expect("Failed!");
+        let mut data_root_ptr: *mut u8 = data_root.as_ptr() as *mut u8;
 
         let data_eroot = CString::new("").expect("Failed!");
-        let mut data_eroot_ptr : *mut u8 = data_eroot.as_ptr() as *mut u8;
+        let mut data_eroot_ptr: *mut u8 = data_eroot.as_ptr() as *mut u8;
 
         let mut ptype = 0 as u64;
         let mut format = 0 as i32;
         let mut length = 0 as u64;
         let mut after = 0 as u64;
 
-        let result = XGetWindowProperty(display, root, atom_root, 0, 1, False, AnyPropertyType as u64, &mut ptype, &mut format, &mut length, &mut after, &mut data_root_ptr);
+        let result = XGetWindowProperty(
+            display,
+            root,
+            atom_root,
+            0,
+            1,
+            False,
+            AnyPropertyType as u64,
+            &mut ptype,
+            &mut format,
+            &mut length,
+            &mut after,
+            &mut data_root_ptr,
+        );
 
         if result == Success as i32 && ptype == XA_PIXMAP {
-            XGetWindowProperty(display, root, atom_eroot, 0, 1, 0, AnyPropertyType as u64, &mut ptype, &mut format, &mut length, &mut after, &mut data_eroot_ptr);
+            XGetWindowProperty(
+                display,
+                root,
+                atom_eroot,
+                0,
+                1,
+                0,
+                AnyPropertyType as u64,
+                &mut ptype,
+                &mut format,
+                &mut length,
+                &mut after,
+                &mut data_eroot_ptr,
+            );
 
             let root_pixmap_id = *(data_root_ptr as *const Pixmap);
             let eroot_pixmap_id = *(data_eroot_ptr as *const Pixmap);
 
-            if ptype == XA_PIXMAP 
-                && root_pixmap_id == eroot_pixmap_id 
-                && pixmap != root_pixmap_id { // Don't kill myself
+            if ptype == XA_PIXMAP && root_pixmap_id == eroot_pixmap_id && pixmap != root_pixmap_id {
+                // Don't kill myself
 
-                println!("Kill client responsible for _XROOTPMAP_ID {}", root_pixmap_id);
+                println!(
+                    "Kill client responsible for _XROOTPMAP_ID {}",
+                    root_pixmap_id
+                );
 
                 XKillClient(display, root_pixmap_id);
                 XFree(data_eroot_ptr as *mut c_void);
@@ -295,9 +392,27 @@ unsafe fn set_root_atoms(display: *mut Display, root: u64, pixmap: Pixmap) -> bo
 
     // setting new background atoms
     let pixmap_ptr: *const Pixmap = &pixmap;
-    
-    XChangeProperty(display, root, atom_root, XA_PIXMAP, 32, PropModeReplace, pixmap_ptr as *const u8, 1);
-    XChangeProperty(display, root, atom_eroot, XA_PIXMAP, 32, PropModeReplace, pixmap_ptr as *const u8, 1);
+
+    XChangeProperty(
+        display,
+        root,
+        atom_root,
+        XA_PIXMAP,
+        32,
+        PropModeReplace,
+        pixmap_ptr as *const u8,
+        1,
+    );
+    XChangeProperty(
+        display,
+        root,
+        atom_eroot,
+        XA_PIXMAP,
+        32,
+        PropModeReplace,
+        pixmap_ptr as *const u8,
+        1,
+    );
 
     return true;
 }
@@ -307,7 +422,7 @@ fn main() {
 
     let options = Arc::new(Options {
         path_to_gif: args.value_of("PATH_TO_GIF").unwrap(),
-        verbose: args.is_present("VERBOSE")
+        verbose: args.is_present("VERBOSE"),
     });
     let running = Arc::new(AtomicBool::new(true));
 
@@ -324,16 +439,15 @@ fn init_args<'a>() -> ArgMatches<'a> {
         .version("0.1")
         .author("Frank Grossgasteiger <frank@grossgasteiger.de>")
         .about("Animates GIF as background in your X-session")
-        .arg(Arg::with_name("VERBOSE")
-            .short("v")
-            .help("Verbose mode"))
-        .arg(Arg::with_name("PATH_TO_GIF")
-            .help("Path to GIF-file")
-            .required(true)
-            .index(1))
+        .arg(Arg::with_name("VERBOSE").short("v").help("Verbose mode"))
+        .arg(
+            Arg::with_name("PATH_TO_GIF")
+                .help("Path to GIF-file")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 }
-
 
 fn init_sigint_handler<'a>(options: Arc<Options<'a>>, running: Arc<AtomicBool>) {
     let verbose = options.verbose;
@@ -344,5 +458,6 @@ fn init_sigint_handler<'a>(options: Arc<Options<'a>>, running: Arc<AtomicBool>) 
         if verbose {
             println!("SIGINT received");
         }
-    }).expect("Error setting Ctrl-C handler");
+    })
+    .expect("Error setting Ctrl-C handler");
 }
