fffc805caad59bf2d008990fcc5d311044130856
Frank Großgasteiger <frank@pixelgerecht.de>
2020-08-30 00:29:08 +0200

    Smaller file

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() {
     println!("cargo:rustc-link-lib=dylib=X11");
     println!("cargo:rustc-link-lib=dylib=Xinerama");
-
-    // For XShm
-    println!("cargo:rustc-link-lib=dylib=Xext");
+    println!("cargo:rustc-link-lib=dylib=Xext"); // For Xshm
 }
