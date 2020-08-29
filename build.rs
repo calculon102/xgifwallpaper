13fd746f2a9123476a11e5b1cf83d85526c19219
Frank Großgasteiger <frank@pixelgerecht.de>
2020-08-29 23:17:54 +0200

    Expcicit dynmic linking

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-lib=X11");
-    println!("cargo:rustc-link-lib=Xinerama");
+    println!("cargo:rustc-link-lib=dylib=X11");
+    println!("cargo:rustc-link-lib=dylib=Xinerama");
 
     // For XShm
-    println!("cargo:rustc-link-lib=Xext");
+    println!("cargo:rustc-link-lib=dylib=Xext");
 }
