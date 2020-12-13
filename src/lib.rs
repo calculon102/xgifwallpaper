267a8adec47b2c0e391892b503145f1add858566
Frank Großgasteiger <frank@grossgasteiger.de>
2020-12-13 13:48:34 +0100

    Set version to 0.4.0-alpha

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ use xcontext::XContext;
 
 const EXIT_INVALID_FILE: i32 = 103;
 
-const VERSION: &str = "0.3.1";
+const VERSION: &str = "0.4.0-alpha";
 
 /// Screens to render wallpapers on, with needed resolution. And the pre-
 /// rendered frames in a seperate map.
